852dfd39eac7ee92e734fcbacc65a3c26cdada6a
CongVuDuc <duccong.vu.2022@smu.edu.sg>
2025-10-06 11:50:53 +0800

    [KIRA-85] fix : nested team assignment under team for team schema

--- backend/src/database/db_setup_tables.py
+++ backend/src/database/db_setup_tables.py
@@ -1,6 +1,7 @@
 from backend.src.database.db_setup import engine, Base
 from backend.src.database.models.task import Task
 from backend.src.database.models.team import Team
+from backend.src.database.models.user import User
 
 # Create tables
 Base.metadata.create_all(engine)
