c55ebaa2a702aa2be72f817750277a095c3bc3ae
CongVuDuc <duccong.vu.2022@smu.edu.sg>
2025-10-09 21:39:39 +0800

    [KIRA-34] test: create test to send email notifications

--- backend/src/services/__init__.py
+++ backend/src/services/__init__.py
@@ -3,10 +3,13 @@ Services module initialization
 """
 from .email_service import EmailService, get_email_service
 from .notification_service import NotificationService, get_notification_service
+from .task_notification_service import TaskNotificationService, get_task_notification_service
 
 __all__ = [
     "EmailService",
     "get_email_service", 
     "NotificationService",
-    "get_notification_service"
+    "get_notification_service",
+    "TaskNotificationService",
+    "get_task_notification_service"
 ]
