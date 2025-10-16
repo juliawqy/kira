7d37035f9514a1fed5f6d4f20634f2b5bb7da8b8
CongVuDuc <duccong.vu.2022@smu.edu.sg>
2025-10-17 00:25:13 +0800

    [KIRA-34] test: rebase and add in in-app notification

--- backend/src/api/v1/router.py
+++ backend/src/api/v1/router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from backend.src.api.v1.routes.task_route import router as task_router
 from backend.src.api.v1.routes.user_route import router as user_router
+from backend.src.api.v1.routes.notification_route import router as notification_router
 
 
 router = APIRouter(prefix="/kira/app/api/v1")
 
 router.include_router(task_router)
 router.include_router(user_router)
+router.include_router(notification_router)
 
