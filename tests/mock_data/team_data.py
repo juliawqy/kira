8ec913e5ba3b1b2aec40e642dcb93e38e024253c
juliawqy <149480793+juliawqy@users.noreply.github.com>
2025-10-23 22:34:10 +0800

    [KIRA-104] chore: update mock data

--- tests/mock_data/team_data.py
+++ tests/mock_data/team_data.py
@@ -34,26 +34,23 @@ DIRECTOR_USER = {
 INVALID_USER_ID = 99999
 
 VALID_TEAM_CREATE = {
+    # "team_id": 1,
+    "team_name": "Cong is the baddie",
+    "manager_id": 2,
+    "department_id": 1,
+    # "team_number": "1",
+}
+
+VALID_TEAM = {
     "team_id": 1,
     "team_name": "Cong is the baddie",
     "manager_id": 2,
     "department_id": 1,
-    "team_number": 1,
+    "team_number": "010100",
 }
 
 
-NO_ROLE_USER = {
-    "user_id": 60,
-    "name": "No Role User",
-    "email": "norole@example.com",
-}
 
 NOT_FOUND_ID = 9999
 
-# Common team ids used in tests
-TEAM_ID_1 = 1
-TEAM_ID_2 = 2
-TEAM_ID_3 = 3
-TEAM_ID_42 = 42
-TEAM_ID_77 = 77
 
